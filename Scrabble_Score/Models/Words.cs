1f24a10a4116668a1ba64ddb725c4cc2adcfbc44
Kiernan2 <kiernan1994@gmail.com>
2022-04-06 13:40:23 -0700

    First commit

---
+++ Scrabble_Score/Models/Words.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Scrabble_Score {
+
+  public class Words
+  {
+    public string[] onePoint = { "a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
+    public string[] twoPoint = { "d", "g"};
+    public string[] threePoint = { "b", "c", "m", "p"};
+    public string[] fourPoint = { "f", "h", "v", "w", "y"};
+    public string[] fivePoint = { "k"};
+    public string[] eightPoint = { "j", "x"};
+    public string[] tenPoint = { "q", "z"};
+    public Words()
+    {
+      
+    }
+  }
+}
---
+++ Scrabble_Score_Tests/Models/WordsTests.cs
@@ -0,0 +1,18 @@
+// using Microsoft.VisualBasic.TestTools.UnitTesting;
+using Scrabble_Score;
+using System.Collections.Generic;
+using System;
+
+namespace Scrabble_Score
+{
+  // [TestClass]
+  // public class GameTests
+  // {
+  //   [TestMethod]
+  //   public void Eleven()
+  //   {
+  //     Word newWord = new Word("QA");
+
+  //   }
+  // }
+}
